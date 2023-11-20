Fixed gradient update problem

src/ai/model.py
@@ -21,6 +21,9 @@ class FF(nn.Module):
         self.fc1 = nn.Linear(self.input_size, 6)
         self.fc2 = nn.Linear(6, 3)
         self.fc3 = nn.Linear(3, self.num_classes)
+        self.fc1.requires_grad_(False)
+        self.fc2.requires_grad_(False)
+        self.fc3.requires_grad_(False)
         if chromosome is not None:
             self.load_chromosome(chromosome)
 
@@ -59,13 +62,12 @@ class FF(nn.Module):
         }
 
     def load_from_dict(self, wb:dict) -> None:
-        with torch.no_grad():
-            self.fc1.weight = torch.nn.parameter.Parameter(wb['w1'])
-            self.fc1.bias = torch.nn.parameter.Parameter(wb['b1'])
-            self.fc2.weight = torch.nn.parameter.Parameter(wb['w2'])
-            self.fc2.bias = torch.nn.parameter.Parameter(wb['b2'])
-            self.fc3.weight = torch.nn.parameter.Parameter(wb['w3'])
-            self.fc3.bias = torch.nn.parameter.Parameter(wb['b3'])
+        self.fc1.weight = torch.nn.parameter.Parameter(wb['w1'])
+        self.fc1.bias = torch.nn.parameter.Parameter(wb['b1'])
+        self.fc2.weight = torch.nn.parameter.Parameter(wb['w2'])
+        self.fc2.bias = torch.nn.parameter.Parameter(wb['b2'])
+        self.fc3.weight = torch.nn.parameter.Parameter(wb['w3'])
+        self.fc3.bias = torch.nn.parameter.Parameter(wb['b3'])
 
     def load_chromosome(self, chromosome:torch.Tensor) -> None:
         self.load_from_dict(self.chromosome2dict(chromosome))

src/train.py
@@ -18,7 +18,7 @@ from .player import FFPlayer, PlayerState
 
 
 class TrainGA:
-    def __init__(self, population_size:int=100):
+    def __init__(self, population_size:int=200):
         # Game configs
         pygame.init()
         pygame.display.set_caption("Flappy Bird")
@@ -80,6 +80,7 @@ class TrainGA:
             # TODO: Fix score display
             # TODO: Animate (matplotlib probably) the best network structure + activations
             # TODO: Create a control slider, to set tick speed
+            # TODO: Increase dificulty
 
             # === Reset game
             await self.reset()
@@ -91,7 +92,7 @@ class TrainGA:
         son_chromosome = [p1_chromosome[gene].item() if selected_parent%2 else p2_chromosome[gene].item() for gene, selected_parent in enumerate(genes_division)]
         return torch.Tensor(son_chromosome).type(p1_chromosome.dtype)
 
-    def mutate(self, chromosome:torch.Tensor, mutation_chance:float=0.1, mutation_std_dev:float=0.01):
+    def mutate(self, chromosome:torch.Tensor, mutation_chance:float=0.05, mutation_std_dev:float=0.3):
         random_selection = np.random.uniform(size=len(chromosome))
         genes_to_mutate = random_selection < mutation_chance
         for mutate, gene in zip(genes_to_mutate, range(len(chromosome))):
@@ -166,7 +167,5 @@ class TrainGA:
         self.score.tick()
         for individual in self.population:
             individual.tick()
-            individual.reset()
         self.config.tick()
         pygame.display.update()
-        #await asyncio.sleep(0)